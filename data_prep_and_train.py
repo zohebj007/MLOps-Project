83d7650e41f9c71a21466c20693666d18ec59596
zohebj007 <jahagirdarzoheb@gmail.com>
2025-12-06 10:52:35 +0530

    test

--- data_prep_and_train.py
+++ data_prep_and_train.py
@@ -1,12 +1,13 @@
 # data_prep_and_train.py
 import pandas as pd
-import numpy as np
+#import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
 from sklearn.preprocessing import StandardScaler
 import joblib
 import os
+#
 
 # --- CONFIG ---
 DATA_PATH = "data/diabetes.csv"   # path to CSV
